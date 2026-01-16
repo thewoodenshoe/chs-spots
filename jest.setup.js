581aad8a167855f9988e3c5aa87e7150b4f9ee3a
Paul Stewart <stewart.paul@gmail.com>
2026-01-15 21:44:33 -0500

    Fix Request/Response mocks in jest.setup.js (require syntax)

--- jest.setup.js
+++ jest.setup.js
@@ -1,2 +1,21 @@
 // Learn more: https://github.com/testing-library/jest-dom
 require('@testing-library/jest-dom');
+
+// Mock global Request/Response for Next.js API routes (Node.js environment)
+if (typeof global.Request === 'undefined') {
+  global.Request = class Request {
+    constructor(input, init) {
+      this.input = input;
+      this.init = init;
+    }
+  };
+}
+
+if (typeof global.Response === 'undefined') {
+  global.Response = class Response {
+    constructor(body, init) {
+      this.body = body;
+      this.init = init;
+    }
+  };
+}
